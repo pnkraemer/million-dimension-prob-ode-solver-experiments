a8bc0288f46103a803eda92a94fd859c6d085f5f
Nicholas Krämer <nicokraemer@gmx.net>
2021-07-11 09:12:05 +0200

    vectorised the formula

--- source/problems.py
+++ source/problems.py
@@ -1,17 +1,38 @@
+"""Test problems."""
 
 import numpy as np
 
 
-__all__ = ["lorenz96"]
+__all__ = ["lorenz96_f", "lorenz96_f_vec"]
 
 
-def lorenz96(t, y, num_variables=5, constant_forcing=8):
+
+def lorenz96_f(t, y, constant_forcing=8):
     """Lorenz 96 model with constant forcing."""
 
     # Setting up vector
+    num_variables = len(y)
     d = np.zeros(num_variables)
 
     # Loops over indices (with operations and Python underflow indexing handling edge cases)
     for i in range(num_variables):
         d[i] = (y[(i + 1) % num_variables] - y[i - 2]) * y[i - 1] - y[i] + constant_forcing
     return d
+
+
+
+
+def lorenz96_f_vec(t, y, constant_forcing=8):
+    """Lorenz 96 model with constant forcing."""
+
+    A = np.roll(y, shift=-1)
+    B = np.roll(y, shift=2)
+    C = np.roll(y, shift=1)
+    D = y
+    return (A - B) * C - D + constant_forcing
+
+
+def lorenz96_df(t, y, constant_forcing=8):
+    """Jacobian of the Lorenz 96 model with constant forcing."""
+
+    raise NotImplementedError
--- tests/test_problems.py
+++ tests/test_problems.py
@@ -9,5 +9,16 @@ import numpy as np
 def test_lorenz96_call():
     y0 = np.arange(10)
     t0 = 2.
-    f0 = problems.lorenz96(t=t0, y=y0, num_variables=10)
+    f0 = problems.lorenz96_f(t=t0, y=y0)
     np.testing.assert_allclose(y0.shape, f0.shape)
+
+
+
+def test_lorenz96_f_vec_and_no_vec():
+    y0 = np.arange(1, 14)
+    t0 = 2.
+
+    f0 = problems.lorenz96_f(t=t0, y=y0)
+    f1 = problems.lorenz96_f_vec(t=t0, y=y0)
+    
+    np.testing.assert_allclose(f0, f1)
