26e6137db889ca7b883ef84ffac1b9977ac797cf
Nicholas Krämer <nicokraemer@gmx.net>
2021-07-11 20:00:55 +0200

    Lorenz plots done

--- source/problems.py
+++ source/problems.py
@@ -12,6 +12,7 @@ config.update("jax_enable_x64", True)
 __all__ = ["lorenz96", "lorenz96_jax", "lorenz96_jax_loop"]
 
 
+
 def lorenz96(t0=0.0, tmax=30.0, y0=None, params=(5, 8.0)):
     """Lorenz 96 system in standard (numpy) implementation."""
 
@@ -39,7 +40,19 @@ def lorenz96_jax(t0=0.0, tmax=30.0, y0=None, params=(5, 8.0)):
     if y0 is None:
         y0 = jnp.ones(num_variables) * constant_forcing
 
+    # Operator representation
+    roll_minus_one = _roll_as_matrix_jax(length=num_variables, shift=-1)
+    roll_two = _roll_as_matrix_jax(length=num_variables, shift=-1)
+    roll_one = _roll_as_matrix_jax(length=num_variables, shift=-1)
+
     def lorenz_rhs(t, y):
+        A = roll_minus_one @ y
+        B = roll_two @ y
+        C = roll_one @ y 
+        D = y
+        return (A - B) * C - D + constant_forcing
+
+    def lorenz_rhs_smartly(t, y):
         A = jnp.roll(y, shift=-1)
         B = jnp.roll(y, shift=2)
         C = jnp.roll(y, shift=1)
@@ -83,3 +96,19 @@ def lorenz96_jax_loop(t0=0.0, tmax=30.0, y0=None, params=(5, 8.0)):
     return pn.problems.InitialValueProblem(
         f=L96, df=lorenz96_df_jax, t0=t0, tmax=tmax, y0=y0
     )
+
+
+
+def _roll_as_matrix(length, shift, axis=0):
+    if axis != 0:
+        raise ValueError
+
+    identity = np.eye(length)
+    return np.roll(identity, shift=shift, axis=0)
+
+def _roll_as_matrix_jax(length, shift, axis=0):
+    if axis != 0:
+        raise ValueError
+
+    identity = jnp.eye(length)
+    return jnp.roll(identity, shift=shift, axis=0)
