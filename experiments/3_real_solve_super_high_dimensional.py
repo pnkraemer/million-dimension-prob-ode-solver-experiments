01445e530135cd624437120435f902ce844c280f
Nicholas Krämer <nicokraemer@gmx.net>
2021-08-12 16:40:41 +0200

    tried a PN solver and gave up again -- the remainders are some comments

--- experiments/3_real_solve_super_high_dimensional.py
+++ experiments/3_real_solve_super_high_dimensional.py
@@ -7,11 +7,12 @@ https://gist.github.com/ChrisRackauckas/cc6ac746e2dfd285c28e0584a2bfd320
 
 
 For the moment, there is no point in running this benchmark with the probabilistic solver as well.
+(I tried, it explodes for some reason on this problem. No idea why.)
 """
 
 import numpy as np
 from scipy.integrate import solve_ivp
-
+from probnum.diffeq import probsolve_ivp
 import timeit
 
 from source import problems
@@ -29,27 +30,30 @@ results = {
     ("RK45", 4): None,
     ("RK45", 8): None,
     ("RK45", 16): None,
-    ("RK45", 32): None,
     ("Radau", 4): None,
     ("Radau", 8): None,
 }
 
 for method, N in results.keys():
 
+
     # Define problem
     problem = problems.advection_diffusion(N)
     f = problem.f
-    t_span = (problem.t0, problem.tmax)
+    t0, tmax = problem.t0, problem.tmax
+
+    t_span = (t0, tmax)
     y0 = problem.y0
 
     # Return the solution once for some statistics
     sol = solve_ivp(f, t_span=t_span, y0=y0, rtol=1e-8, atol=1e-8, method=method)
+    num_steps = len(sol.t)
+
 
     def time_func():
         solve_ivp(f, t_span=t_span, y0=y0, rtol=1e-8, atol=1e-8, method=method)
 
     timed_solve = timeit.Timer(time_func).timeit(number=1)  # in seconds
-    num_steps = len(sol.t)
     time_per_step = timed_solve/num_steps
 
     results[(method, N)] = (time_per_step, num_steps)
