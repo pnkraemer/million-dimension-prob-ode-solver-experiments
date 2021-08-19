f24891e6bc00700ea38b486d98925e1bd89918b5
Nicholas Krämer <nicokraemer@gmx.net>
2021-08-19 18:38:16 +0200

    filter sol

--- experiments/4_stiff_brusselator.py
+++ experiments/4_stiff_brusselator.py
@@ -11,21 +11,48 @@ import jax.numpy as jnp
 
 from matplotlib import cm
 
+import jax
+
+
+def extract_filter(N):
+    ivp = tornado.ivp.brusselator(N=N, tmax=10.0)
+    odefilter_sol, solver = tornado.ivpsolve.solve(
+        ivp=ivp,
+        method="ek1_truncated",
+        adaptive=True,
+        save_every_step=False,
+        num_derivatives=3,
+        abstol=1e-2,
+        reltol=1e-2,
+    )
+    return (solver.P0 @ jnp.stack(odefilter_sol.y.mean))[:N]
+
+
+def extract_scipy(N):
+    ivp = tornado.ivp.brusselator(N=N, tmax=10.0)
+
+    lsoda_sol = scipy.integrate.solve_ivp(
+        ivp.f, t_span=(ivp.t0, ivp.tmax), y0=ivp.y0, method="LSODA"
+    )
+    return lsoda_sol.y[:N, -1]
+
 
 for N in [10, 20, 50, 100]:
-    ivp = tornado.ivp.brusselator(N=N)
 
-    sol = scipy.integrate.solve_ivp(
-        ivp.f, t_span=(ivp.t0, ivp.tmax), y0=ivp.y0, method="RK45"
-    )
+    y_ref = extract_scipy(N)
+    y = extract_filter(N)
 
-    x = jnp.arange(1, N + 1) / N
-    t = sol.t
+    print(jnp.linalg.norm(y_ref - y) / jnp.sqrt(y.size))
 
-    T, X = jnp.meshgrid(t, x)
-    U = sol.y[:N]
+    # x = jnp.arange(1, N + 1) / N
+    # t = jnp.stack(sol.t)
 
-    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-    ax.plot_surface(T, X, U, cmap=cm.viridis)
-    ax.set_title(f"d={N}, N={len(sol.t)}")
-    plt.show()
+    # T, X = jnp.meshgrid(t, x)
+    # U = jnp.stack(sol.mean)[:, :N].T
+
+    # assert T.shape == X.shape == U.shape, (T.shape, X.shape, U.shape)
+
+    # fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    # ax.plot_surface(T, X, U, cmap=cm.viridis)
+    # ax.set_title(f"d={N}, N={len(sol.t)}")
+    # plt.show()
