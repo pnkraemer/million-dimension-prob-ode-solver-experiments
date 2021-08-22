3df20f431b82ee7b5de8ad5002c7f6a9e1579d05
Nicholas Krämer <nicokraemer@gmx.net>
2021-08-22 13:00:51 +0200

    stiff brusselator experiment

--- experiments/4_stiff_brusselator.py
+++ experiments/4_stiff_brusselator.py
@@ -1,4 +1,12 @@
-"""Stiff brusselator."""
+"""Stiff brusselator. Can we solve it as successfully with the truncated EK1 as with the full EK1?
+
+This questions shall answer this in terms of answering two questions:
+1) does the truncated ek1 solver -- like the non-truncated ek1 solver and non-probabilistic, implicit methods -- require constant number of steps independent of the dimension (which for the brusselator can be translated to: independent of stiffness) to achieve prescribed accuracy?
+2) Does it do so while scaling better with the size of the problem than the reference solver?
+
+If the answer is "yes" to both problems, the truncated solver is a success.
+Then, it will be interesting to find out at which dimensionality one should start using the truncated version.
+"""
 
 
 import tornado
@@ -14,45 +22,77 @@ from matplotlib import cm
 import jax
 
 
-def extract_filter(N):
-    ivp = tornado.ivp.brusselator(N=N, tmax=10.0)
-    odefilter_sol, solver = tornado.ivpsolve.solve(
-        ivp=ivp,
-        method="ek1_truncated",
-        adaptive=True,
-        save_every_step=False,
-        num_derivatives=3,
-        abstol=1e-2,
-        reltol=1e-2,
+import time
+
+
+def solve(ivp, solver):
+    for idx, state in enumerate(solver.solution_generator(ivp)):
+        pass
+    if isinstance(solver, tornado.ek1.ReferenceEK1):
+        return solver.P0 @ state.y.mean, idx
+    return state.y.mean[0], idx
+
+
+def solve_scipy(ivp, tolerance):
+    radau_sol = scipy.integrate.solve_ivp(
+        ivp.f,
+        t_span=(ivp.t0, ivp.tmax),
+        y0=ivp.y0,
+        method="Radau",
+        atol=tolerance,
+        rtol=tolerance,
     )
-    return (solver.P0 @ jnp.stack(odefilter_sol.y.mean))[:N]
+    return radau_sol.y[:, -1], len(radau_sol.t)
 
 
-def extract_scipy(N):
-    ivp = tornado.ivp.brusselator(N=N, tmax=10.0)
-
-    lsoda_sol = scipy.integrate.solve_ivp(
-        ivp.f, t_span=(ivp.t0, ivp.tmax), y0=ivp.y0, method="LSODA"
-    )
-    return lsoda_sol.y[:N, -1]
-
-
+# Careful with the large values here -- the benchmark will take a while
 for N in [10, 20, 50, 100]:
+    bruss = tornado.ivp.brusselator(N=N)
+    t1 = time.time()
+    y_ref, num_steps_ref = solve_scipy(bruss, tolerance=1e-10)
+    t_ref = time.time() - t1
+    for tol in [1e-1, 1e-3, 1e-5, 7]:
 
-    y_ref = extract_scipy(N)
-    y = extract_filter(N)
+        print(f"Results for N={N}, tol={tol}:")
 
-    print(jnp.linalg.norm(y_ref - y) / jnp.sqrt(y.size))
+        # Set up solvers
+        steps = tornado.step.AdaptiveSteps(0.1, tol, tol)
 
-    # x = jnp.arange(1, N + 1) / N
-    # t = jnp.stack(sol.t)
+        # Truncated EK1
+        for n in [3, 8]:
+            solver = tornado.ek1.TruncationEK1(
+                num_derivatives=n, ode_dimension=bruss.dimension, steprule=steps
+            )
+            t1 = time.time()
+            y_pn, num_steps_pn = solve(ivp=bruss, solver=solver)
+            t_pn = time.time() - t1
+            error_pn = jnp.linalg.norm(y_pn - y_ref) / jnp.sqrt(y_ref.size)
 
-    # T, X = jnp.meshgrid(t, x)
-    # U = jnp.stack(sol.mean)[:, :N].T
+            # Reference EK1
+            solver_dense = tornado.ek1.ReferenceEK1(
+                num_derivatives=n, ode_dimension=bruss.dimension, steprule=steps
+            )
+            t1 = time.time()
+            y_dense, num_steps_dense = solve(ivp=bruss, solver=solver_dense)
+            t_dense = time.time() - t1
+            error_dense = jnp.linalg.norm(y_dense - y_ref) / jnp.sqrt(y_ref.size)
 
-    # assert T.shape == X.shape == U.shape, (T.shape, X.shape, U.shape)
+            print(
+                f"\tTruncatedEK1 (nu={n}): error={error_pn}, N={num_steps_pn}, t={t_pn} sec (t/N={t_pn/num_steps_pn} sec)"
+            )
+            print(
+                f"\tReferenceEK1 (nu={n}): error={error_dense}, N={num_steps_dense}, t={t_dense} sec (t/N={t_dense/num_steps_dense} sec)"
+            )
 
-    # fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-    # ax.plot_surface(T, X, U, cmap=cm.viridis)
-    # ax.set_title(f"d={N}, N={len(sol.t)}")
-    # plt.show()
+        # Scipy (Radau)
+        t1 = time.time()
+        y_scipy, num_steps_scipy = solve_scipy(bruss, tolerance=tol)
+        t_scipy = time.time() - t1
+        error_scipy = jnp.linalg.norm(y_scipy - y_ref) / jnp.sqrt(y_ref.size)
+
+        print(
+            f"\tScipy (Radau): error={error_scipy}, N={num_steps_scipy}, t={t_scipy} sec (t/N={t_scipy/num_steps_scipy} sec)"
+        )
+        print()
+
+    print()
