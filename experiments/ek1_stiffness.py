0c867e8c36cb62d1258e52fd63cbfb024fb87c93
Nathanael Bosch <nathanael.bosch@gmail.com>
2021-09-10 11:46:01 +0200

    Add experiment + plotting comparing solvers across VdP stiffness

---
+++ experiments/ek1_stiffness.py
@@ -0,0 +1,134 @@
+"""
+Goal: Compare the truncated EK1 to the full EK1
+
+Ideally, either show:
+- the truncated version has worse stability properties
+  => show how it fails on the stiff VdP
+- the truncated version has basically the same stability properties
+  => work-precision diagram to show how they are the same
+"""
+import pathlib
+import time
+
+import jax
+from jax import numpy as jnp
+import scipy.integrate
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from tqdm import tqdm
+
+import tornadox
+from tornadox.ek0 import *
+from tornadox.ek1 import *
+
+
+def myvanderpol(t0=0.0, tmax=6.3, y0=None, stiffness_constant=1e1):
+
+    y0 = y0 or jnp.array([2.0, 0.0])
+
+    @jax.jit
+    def f_vanderpol(_, Y, mu=stiffness_constant):
+        return jnp.array([Y[1], mu * ((1.0 - Y[0] ** 2) * Y[1] - Y[0])])
+
+    df_vanderpol = jax.jit(jax.jacfwd(f_vanderpol, argnums=1))
+
+    @jax.jit
+    def df_diagonal_vanderpol(_, Y, mu=stiffness_constant):
+        return jnp.array([0.0, mu * (1.0 - Y[0] ** 2)])
+
+    return tornadox.ivp.InitialValueProblem(
+        f=f_vanderpol,
+        t0=t0,
+        tmax=tmax,
+        y0=y0,
+        df=df_vanderpol,
+        df_diagonal=df_diagonal_vanderpol,
+    )
+
+
+MUS = 10 ** jnp.arange(6)
+
+ATOL, RTOL = 1e-6, 1e-3
+STEPRULE = tornadox.step.AdaptiveSteps(abstol=ATOL, reltol=RTOL, min_step=1e-10)
+
+ORDER = 5
+SOLVERS = [
+    ("ReferenceEK0", ReferenceEK0(num_derivatives=ORDER, steprule=STEPRULE)),
+    ("KroneckerEK0", KroneckerEK0(num_derivatives=ORDER, steprule=STEPRULE)),
+    ("DiagonalEK0", DiagonalEK0(num_derivatives=ORDER, steprule=STEPRULE)),
+    ("ReferenceEK1", ReferenceEK1(num_derivatives=ORDER, steprule=STEPRULE)),
+    ("DiagonalEK1", DiagonalEK1(num_derivatives=ORDER, steprule=STEPRULE)),
+    ("ETruncationEK1", EarlyTruncationEK1(num_derivatives=ORDER, steprule=STEPRULE)),
+]
+
+
+def reference_solve(vdp):
+    print("Radau solve")
+    radau_sol = scipy.integrate.solve_ivp(
+        vdp.f,
+        t_span=(vdp.t0, vdp.tmax),
+        y0=vdp.y0,
+        method="Radau",
+        atol=1e-10,
+        rtol=1e-10,
+    )
+    print(f"SOLVED ({len(radau_sol.t)} steps)")
+    return radau_sol
+
+
+def solve_vdp_and_save_results(
+    name, solver, vdp, reference_solution, result_dict, totry
+):
+    print(f"{name} start")
+    if totry[name]:
+        try:
+            start = time.time()
+            solution = solver.solve(vdp)
+            seconds = time.time() - start
+            nsteps = len(solution.t)
+            error = jnp.linalg.norm(solution.mean[-1][0] - reference_solution.y[:, -1])
+            print(f"SOLVED ({nsteps} steps)")
+        except ValueError:
+            print("FAILED")
+            totry[name] = False
+            nsteps, error, seconds = None, None, None
+    else:
+        nsteps, error, seconds = None, None, None
+    result_dict[f"{name}_nsteps"].append(nsteps)
+    result_dict[f"{name}_errors"].append(error)
+    result_dict[f"{name}_seconds"].append(seconds)
+
+
+def main():
+    totry = {n: True for (n, _) in SOLVERS}
+    result_dict = {"mu": MUS}
+    for n, _ in SOLVERS:
+        result_dict[f"{n}_nsteps"] = []
+        result_dict[f"{n}_errors"] = []
+        result_dict[f"{n}_seconds"] = []
+
+    for mu in tqdm(MUS):
+        vdp = myvanderpol(stiffness_constant=mu)
+
+        reference_solution = reference_solve(vdp)
+
+        for name, solver in SOLVERS:
+            solve_vdp_and_save_results(
+                name,
+                solver,
+                vdp,
+                reference_solution,
+                result_dict,
+                totry,
+            )
+
+    RESULT_DIR = pathlib.Path("./results/ek1_stiffness")
+    if not RESULT_DIR.is_dir():
+        RESULT_DIR.mkdir(parents=True)
+    df = pd.DataFrame(result_dict)
+    df.to_csv(RESULT_DIR / "results.csv", sep=";", index=False)
+
+
+if __name__ == "__main__":
+    main()
---
+++ plotting_scripts/ek1_stiffness.py
@@ -0,0 +1,54 @@
+import pathlib
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+path = pathlib.Path("./results/ek1_stiffness/results.csv")
+df = pd.read_csv(path, sep=";")
+
+
+SOLVERS_TO_PLOT = [
+    "ReferenceEK0",
+    "KroneckerEK0",
+    "DiagonalEK0",
+    "ReferenceEK1",
+    "DiagonalEK1",
+    "ETruncationEK1",
+]
+
+
+# Plot the number of steps for different stiffnesses
+for s in SOLVERS_TO_PLOT:
+    plt.plot(df.mu, df[f"{s}_nsteps"], "o-", label=s)
+plt.yscale("log")
+plt.xscale("log")
+plt.ylabel("Number of steps")
+plt.xlabel("Stiffness constant")
+plt.legend()
+# plt.savefig("stability.png")
+plt.show()
+
+
+# Plot the error for different stiffnesses
+for s in SOLVERS_TO_PLOT:
+    plt.plot(df.mu, df[f"{s}_errors"], "o-", label=s)
+plt.yscale("log")
+plt.xscale("log")
+plt.ylabel("Error")
+plt.xlabel("Stiffness constant")
+plt.legend()
+# plt.savefig("errors.png")
+plt.show()
+
+
+# Plot seconds
+for s in SOLVERS_TO_PLOT:
+    plt.plot(df.mu, df[f"{s}_seconds"], "o-", label=s)
+plt.yscale("log")
+plt.xscale("log")
+plt.ylabel("Seconds")
+plt.xlabel("Stiffness constant")
+plt.legend()
+# plt.savefig("errors.png")
+plt.show()
