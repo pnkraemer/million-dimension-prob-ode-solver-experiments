99450a89e274370e061f8932ca55b24c719b6203
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2021-09-15 19:46:03 +0200

    More work-in-progress for experiment 3

--- experiments/3_real_solve_super_high_dimensional.py
+++ experiments/3_real_solve_super_high_dimensional.py
@@ -3,38 +3,48 @@
 The goal is to show that with all the changes, high-dimensional ODE solvers are *possible.*
 """
 import tornadox
+import jax.numpy as jnp
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 
 ########################################################################################
 # Reference
-# IVP = tornadox.ivp.fhn_2d(dx=0.05) # works
+# IVP = tornadox.ivp.fhn_2d(dx=0.1)  # works
 # IVP = tornadox.ivp.fhn_2d() # works
 # IVP = tornadox.ivp.fhn_2d(bbox=[[-1.0, -1.0], [1.0, 1.0]], dx=0.05)
 # IVP = tornadox.ivp.fhn_2d(bbox=[[-2.0, -2.0], [2.0, 2.0]], dx=0.05)
-IVP = tornadox.ivp.fhn_2d(bbox=[[-2.0, -2.0], [2.0, 2.0]], dx=0.04)
-# IVP = tornadox.ivp.fhn_2d(bbox=[[-2.0, -2.0], [2.0, 2.0]], dx=0.025)
+# IVP = tornadox.ivp.fhn_2d(bbox=[[-2.0, -2.0], [2.0, 2.0]], dx=0.04)
+# IVP = tornadox.ivp.fhn_2d(bbox=[[-2.0, -2.0], [2.0, 2.0]], dx=0.02)
+IVP = tornadox.ivp.fhn_2d(bbox=[[-2.0, -2.0], [2.0, 2.0]], dx=0.01)
+len(IVP.y0)
 
 
 ########################################################################################
 # Reference
-reference_sol = solve_ivp(
-    fun=IVP.f, t_span=(IVP.t0, IVP.tmax), y0=IVP.y0, atol=1e-10, rtol=1e-10
-)
+# reference_sol = solve_ivp(
+#     fun=IVP.f, t_span=(IVP.t0, IVP.tmax), y0=IVP.y0, atol=1e-10, rtol=1e-10
+# )
 
 
 ########################################################################################
 # Ours
-steprule = tornadox.step.AdaptiveSteps(abstol=1e-3, reltol=1e-3)
-# solver = tornadox.ek0.DiagonalEK0(
-# solver = tornadox.ek1.ReferenceEK1(
-# solver = tornadox.ek0.ReferenceEK0(
+steprule = tornadox.step.AdaptiveSteps(abstol=1e-3, reltol=1e-2)
 solver = tornadox.ek0.KroneckerEK0(
+    num_derivatives=3,
     steprule=steprule,
     initialization=tornadox.init.RungeKutta(use_df=False),
 )
-# solution = solver.solve(IVP, save_covariances=False, progressbar=True)
-state, info = solver.simulate_final_state(IVP, progressbar=True)
+# Solve:
+saveat = jnp.arange(1, 20)  # 20 == IVP.tmax
+ts, ys = [], []
+for state, info in solver.solution_generator(IVP, stop_at=saveat, progressbar=True):
+    if state.t in saveat:
+        print(f"saving! t={state.t}")
+        ts.append(state.t)
+        ys.append(state.y.mean[0, :])
+print(f"saving! t={state.t}")
+ts.append(state.t)
+ys.append(state.y.mean[0, :])
 
 
 ########################################################################################
@@ -49,10 +59,12 @@ def plot_y(y):
 
 
 # Plot the reference solution
-plot_y(reference_sol.y[:, -1])
-plt.title("RK45")
+# fig = plot_y(reference_sol.y[:, -1])
+# plt.title("RK45")
+# fig.savefig("RK45.png")
 
 # Plot our solution
-y = plot_y(state.y.mean[0, :])
+fig = plot_y(means[-1][0, :])
 plt.title("KroneckerEK0")
 plt.show()
+# fig.savefig("KroneckerEK0.png")
