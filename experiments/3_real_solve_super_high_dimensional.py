eac5301ba0099b8b2c864fb75aee57c024c16068
Nathanael Bosch <nathanael.bosch@gmail.com>
2021-09-15 10:50:23 +0200

    Solve the fhn_2d PDE for experiment 3

--- experiments/3_real_solve_super_high_dimensional.py
+++ experiments/3_real_solve_super_high_dimensional.py
@@ -1,62 +1,23 @@
 """This experiment evaluates whether one can 'properly solve' a high-dimensional problem.
 
 The goal is to show that with all the changes, high-dimensional ODE solvers are *possible.*
-
-The example is taken from:
-https://gist.github.com/ChrisRackauckas/cc6ac746e2dfd285c28e0584a2bfd320
-
-
-For the moment, there is no point in running this benchmark with the probabilistic solver as well.
-(I tried, it explodes for some reason on this problem. No idea why.)
 """
-
-import timeit
-
-import numpy as np
-import tqdm
+import tornadox
 from scipy.integrate import solve_ivp
 
-from hose import problems
+# IVP = tornadox.ivp.fhn_2d(dx=0.05)
+IVP = tornadox.ivp.fhn_2d()
+IVP = tornadox.ivp.fhn_2d(bbox=[[-1.0, -1.0], [1.0, 1.0]], dx=0.05)
+REFERENCE_SOLUTION = solve_ivp(
+    fun=IVP.f, t_span=(IVP.t0, IVP.tmax), y0=IVP.y0, atol=1e-10, rtol=1e-10
+)
 
-sec_to_ms = lambda t: 1000 * t
-
-
-# Prepare results in a dict
-results = {
-    ("RK45", 4): None,
-    ("RK45", 8): None,
-    ("RK45", 16): None,
-    ("Radau", 4): None,
-    ("Radau", 8): None,
-}
-
-for method, N in tqdm.tqdm(results.keys()):
-
-    # Define problem
-    problem = problems.advection_diffusion(N)
-    f = problem.f
-    t0, tmax = problem.t0, problem.tmax
-
-    t_span = (t0, tmax)
-    y0 = problem.y0
-
-    # Return the solution once for some statistics
-    sol = solve_ivp(f, t_span=t_span, y0=y0, rtol=1e-8, atol=1e-8, method=method)
-    num_steps = len(sol.t)
-
-    def time_func():
-        solve_ivp(f, t_span=t_span, y0=y0, rtol=1e-8, atol=1e-8, method=method)
-
-    timed_solve = timeit.Timer(time_func).timeit(number=1)  # in seconds
-    time_per_step = timed_solve / num_steps
-
-    results[(method, N)] = (time_per_step, num_steps)
-
-
-# Print the results
-for method, N in results.keys():
-    time_per_step, num_steps = results[(method, N)]
-    print()
-    print(f"METHOD={method}, N={N} ({3*N*N}-dimensional problem)")
-    print(f"\ttime_per_step={sec_to_ms(time_per_step)} ms ({num_steps} steps)")
-    print()
+steprule = tornadox.step.AdaptiveSteps(abstol=1e-3, reltol=1e-3)
+# solver = tornadox.ek0.DiagonalEK0(
+# solver = tornadox.ek1.ReferenceEK1(
+# solver = tornadox.ek0.ReferenceEK0(
+solver = tornadox.ek0.KroneckerEK0(
+    steprule=steprule,
+    initialization=tornadox.init.RungeKutta(use_df=False),
+)
+solution = solver.solve(IVP, progressbar=True)
