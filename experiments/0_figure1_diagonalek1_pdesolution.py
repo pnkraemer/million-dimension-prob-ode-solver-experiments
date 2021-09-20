4d44fff9c227d59d28f2b7420a8b0bdfb279258b
Nathanael Bosch <nathanael.bosch@gmail.com>
2021-09-20 13:39:53 +0200

    Generate plots for Figure 1

---
+++ experiments/0_figure1_diagonalek1_pdesolution.py
@@ -0,0 +1,67 @@
+import pathlib
+import tornadox
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+
+
+# Specify the IVP
+IVP = tornadox.ivp.fhn_2d(bbox=[[-1.0, -1.0], [1.0, 1.0]], dx=0.02)
+D = len(IVP.y0)
+print(f"Dimension: {D}")
+
+
+# Specify the solver
+steprule = tornadox.step.AdaptiveSteps(abstol=1e-3, reltol=1e-1)
+solver = tornadox.ek1.DiagonalEK1(
+    num_derivatives=2,
+    steprule=steprule,
+    initialization=tornadox.init.RungeKutta(use_df=False),
+)
+
+
+# Solve the problem
+saveat = [2, 5, 10, 15, 20]
+ts, y_means, y_vars = [], [], []
+for state, info in solver.solution_generator(IVP, stop_at=saveat, progressbar=True):
+    if state.t in saveat:
+        print(f"saving! t={state.t}")
+        ts.append(state.t)
+        y_means.append(state.y.mean[0, :])
+        y_vars.append(state.y.cov[:, 0, 0])
+ts.append(state.t)
+y_means.append(state.y.mean[0, :])
+y_vars.append(state.y.cov[:, 0, 0])
+y_stds = [jnp.sqrt(v) for v in y_vars]
+ts = jnp.stack(ts)
+y_means = jnp.stack(y_means)
+y_stds = jnp.stack(y_stds)
+
+
+# Save the solution
+result_dir = pathlib.Path("./results/0_figure1_diagonalek1_pdesolution")
+result_dir.mkdir(parents=True, exist_ok=True)
+jnp.save(result_dir / "times.npy", ts)
+jnp.save(result_dir / "means.npy", y_means)
+jnp.save(result_dir / "stddevs.npy", y_stds)
+
+
+# Visualize
+d = D // 2
+_d = int(d ** (1 / 2))
+# Plot means
+for _t, _y in zip(ts, y_means):
+    fig = plt.figure()
+    cm = plt.imshow(_y[:d].reshape(_d, _d), vmin=-1, vmax=1)
+    plt.axis("off")
+    fig.colorbar(cm, extend="both")
+    fig.tight_layout()
+    fig.savefig(result_dir / f"mean_t={_t}.png")
+# Plot stddevs
+for _t, _y in zip(ts, y_stds):
+    fig = plt.figure()
+    cm = plt.imshow(_y[:d].reshape(_d, _d), vmin=0)
+    plt.axis("off")
+    fig.colorbar(cm, extend="max")
+    fig.tight_layout()
+    fig.savefig(result_dir / f"stddev_t={_t}.png")
+plt.close("all")
